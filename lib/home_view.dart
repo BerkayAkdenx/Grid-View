53e115589fced6b01bfe49c58b99d3a9927c7d61
BerkayAkdenx <ismetberkayakdeniz@gmail.com>
2023-02-28 16:22:32 +0300

    v2

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -13,23 +13,41 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   List<String> pageNames = ['BasketBoll', 'Futbol', 'Tenis'];
   List<Widget> pages = [BasketballView(), SoccerView(), TennisView()];
+  final List<int> _selectedItems = [];
+
+  void _toggleSelection(int index) {
+    setState(() {
+      if (_selectedItems.contains(index)) {
+        _selectedItems.remove(index);
+      } else {
+        _selectedItems.add(index);
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: GridView.builder(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            scrollDirection: Axis.horizontal,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 2, childAspectRatio: 2 / 1),
             itemCount: (pageNames.length),
             itemBuilder: (context, index) {
               return GestureDetector(
                 onTap: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => pages[index]));
+                  // Navigator.push(context,MaterialPageRoute(builder: (context) => pages[index]));
+                  _toggleSelection(index);
                 },
                 child: Card(
+                  color: _selectedItems.contains(index)
+                      ? Colors.blue.withOpacity(0.5)
+                      : Colors.white,
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
-                    children: [Text(pageNames[index])],
+                    children: [
+                      Text(pageNames[index]),
+                    ],
                   ),
                 ),
               );
--- lib/soccer_view.dart
+++ lib/soccer_view.dart
@@ -12,7 +12,10 @@ class _SoccerViewState extends State<SoccerView> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Image.network('https://image.api.playstation.com/vulcan/img/rnd/202111/0822/zDXM9K2cQiq0vKTDwF0TkAor.png', fit :BoxFit.cover),
-      ),);
+        child: Image.network(
+            'https://image.api.playstation.com/vulcan/img/rnd/202111/0822/zDXM9K2cQiq0vKTDwF0TkAor.png',
+            fit: BoxFit.cover),
+      ),
+    );
   }
 }
